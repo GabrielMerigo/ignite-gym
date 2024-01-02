df583d59ba39499d63cc9b0d60481db8956355ce
Gabriel <gabriel.merigo.dev@gmail.com>
2024-01-02 19:10:23 -0300

    more accs

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -6,7 +6,7 @@ import { Input } from '@components/Input';
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg={'gray.700'}>
+    <VStack flex={1} bg={'gray.700'} px={10}>
       <Image
         resizeMode="contain"
         source={BackgroundImg}
@@ -27,8 +27,12 @@ export function SignIn() {
         </Heading>
       </Center>
 
-      <Input placeholder="E-mail" />
-      <Input placeholder="Senha" />
+      <Input
+        placeholder="E-mail"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <Input placeholder="Senha" secureTextEntry />
     </VStack>
   );
 }
