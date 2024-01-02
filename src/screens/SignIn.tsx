278b06b573010d4a7eec237087e3aefbce34c5dd
Gabriel <gabriel.merigo.dev@gmail.com>
2024-01-02 19:04:03 -0300

    styling field

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,6 +2,7 @@ import { VStack, Image, Text, Center, Heading } from 'native-base';
 
 import LogoImg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
+import { Input } from '@components/Input';
 
 export function SignIn() {
   return (
@@ -25,6 +26,8 @@ export function SignIn() {
           Acesse sua conta
         </Heading>
       </Center>
+
+      <Input placeholder="E-mail" />
     </VStack>
   );
 }
