2b61a6f2e115b05cc3cf605dedf4aaed706b65dd
Gabriel <gabriel.merigo.dev@gmail.com>
2024-01-02 19:28:45 -0300

    Criado vriants button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,19 +4,25 @@ type ButtonProps = IButtonProps & {
   title: string;
 };
 
-export function Button({ title, ...rest }: ButtonProps) {
+export function Button({ title, variant, ...rest }: ButtonProps) {
   return (
     <NativeBaseButton
-      bg="green.700"
+      bg={variant === 'outline' ? 'transparent' : 'green.700'}
+      borderWidth={variant === 'outline' ? 1 : 0}
+      borderColor="green.500"
       h={14}
       w="full"
       rounded="sm"
       _pressed={{
-        background: 'green.500'
+        background: variant === 'outline' ? 'gray.500' : 'green.500'
       }}
       {...rest}
     >
-      <Text color="white" fontFamily="heading" fontSize="sm">
+      <Text
+        color={variant === 'outline' ? 'green.500' : 'white'}
+        fontFamily="heading"
+        fontSize="sm"
+      >
         {title}
       </Text>
     </NativeBaseButton>
